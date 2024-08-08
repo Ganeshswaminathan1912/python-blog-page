00fe975d3b09244d767cb44d5f03a3beaf44deff
Ganesh Swaminathan K <64898189+Ganeshswaminathan1912@users.noreply.github.com>
2024-08-08 09:43:06 +0530

    commit 5 - AI

---
+++ blog-frontend/src/components/PostList.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const PostList = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const res = await axios.get('http://localhost:8000/api/posts/');
+      setPosts(res.data);
+    };
+    fetchPosts();
+  }, []);
+
+  return (
+    <div>
+      <h1>Blog Posts</h1>
+      <ul>
+        {posts.map(post => (
+          <li key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.content}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default PostList;
